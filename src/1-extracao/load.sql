a2913f2efdf19d48c29e900324c6ad70f78960ea
Antonio <antoniomanoelss2025@gmail.com>
2026-08-07 22:01:20 +0000

    duckdbf

--- src/1-extracao/load.sql
+++ src/1-extracao/load.sql
@@ -4,3 +4,10 @@ SELECT * FROM 'data/raw/yellow_tripdata_2024-01.parquet';
 
 CREATE OR REPLACE TABLE zones_raw AS
 SELECT * FROM 'data/raw/taxi_zone_lookup.csv';
+
+/* wget https://github.com/duckdb/duckdb/releases/download/v1.1.3/duckdb_cli-linux-amd64.zip
+unzip duckdb_cli-linux-amd64.zip
+rm duckdb_cli-linux-amd64.zip 
+
+codigo para baixar duckdb
+*/
